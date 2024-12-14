7a67b02af6cca4003f80be9f42c29589357b44cc
hyper <39167664+heypr@users.noreply.github.com>
2024-12-13 21:02:04 -0500

    Removed unused import

--- src/main/java/dev/heypr/mythicinventories/commands/OpenInventoryCommand.java
+++ src/main/java/dev/heypr/mythicinventories/commands/OpenInventoryCommand.java
@@ -6,7 +6,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
 
 public class OpenInventoryCommand implements CommandExecutor {
